538bf8246e68a2b877a50e1d115b9c0db09eab9e
Leo Dion <leogdion@brightdigit.com>
2025-06-23 16:36:54 -0400

    fixing unwrap

--- Sources/SyntaxKit/Expressions/Closure.swift
+++ Sources/SyntaxKit/Expressions/Closure.swift
@@ -7,7 +7,7 @@
 //
 //  Permission is hereby granted, free of charge, to any person
 //  obtaining a copy of this software and associated documentation
-//  files (the “Software”), to deal in the Software without
+//  files (the "Software"), to deal in the Software without
 //  restriction, including without limitation the rights to use,
 //  copy, modify, merge, publish, distribute, sublicense, and/or
 //  sell copies of the Software, and to permit persons to whom the
@@ -17,7 +17,7 @@
 //  The above copyright notice and this permission notice shall be
 //  included in all copies or substantial portions of the Software.
 //
-//  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 //  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 //  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 //  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
@@ -125,6 +125,14 @@ public struct Closure: CodeBlock {
       )
     }
 
+    // Prepare return clause safely
+    let returnClause: ReturnClauseSyntax? = returnType.map {
+      ReturnClauseSyntax(
+        arrow: .arrowToken(trailingTrivia: .space),
+        type: $0.typeSyntax
+      )
+    }
+
     let signature: ClosureSignatureSyntax? =
       (parameters.isEmpty && returnType == nil && capture.isEmpty && attributes.isEmpty)
       ? nil
@@ -171,12 +179,7 @@ public struct Closure: CodeBlock {
             )
           ),
         effectSpecifiers: nil,
-        returnClause: returnType == nil
-          ? nil
-          : ReturnClauseSyntax(
-            arrow: .arrowToken(trailingTrivia: .space),
-            type: returnType!.typeSyntax
-          ),
+        returnClause: returnClause,
         inKeyword: .keyword(.in, leadingTrivia: .space, trailingTrivia: .space)
       )
 
