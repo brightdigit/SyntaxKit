0b6f191c30fbd573599b628469458f7b7fc1fc1f
Leo Dion <leogdion@brightdigit.com>
2025-06-27 08:41:37 -0400

    starting work on generics

--- Examples/Remaining/generics/code.swift
+++ Examples/Remaining/generics/code.swift
@@ -1,3 +1,12 @@
+protocol Stackable {
+    associatedtype Element : Hashable & Identifiable
+
+    mutating func push(_ item: Element)
+    mutating func pop() -> Element?
+    func peek() -> Element?
+    var isEmpty: Bool { get }
+    var count: Int { get }
+}
 struct Stack<Element> {
     private var items: [Element] = []
 
@@ -19,5 +28,11 @@ struct Stack<Element> {
 
     var count: Int {
         items.count
+    }
+}
+
+enum Noop {
+    static func nothing(_ stack: any Stackable) -> any Stackable { 
+        return stack
     }
 }
