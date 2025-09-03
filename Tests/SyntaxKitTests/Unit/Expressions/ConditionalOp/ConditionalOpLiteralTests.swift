f2becd36cc97e0b6fca69386c8635ffa122d8d5d
Leo Dion <leogdion@brightdigit.com>
2025-09-03 08:31:03 -0400

    fixing unit tests

--- Tests/SyntaxKitTests/Unit/Expressions/ConditionalOp/ConditionalOpLiteralTests.swift
+++ Tests/SyntaxKitTests/Unit/Expressions/ConditionalOp/ConditionalOpLiteralTests.swift
@@ -27,6 +27,7 @@
 //  OTHER DEALINGS IN THE SOFTWARE.
 //
 
+import Foundation
 import SwiftSyntax
 import Testing
 
--- Tests/SyntaxKitTests/Unit/Expressions/LiteralValueTests.swift
+++ Tests/SyntaxKitTests/Unit/Expressions/LiteralValueTests.swift
@@ -27,6 +27,7 @@
 //  OTHER DEALINGS IN THE SOFTWARE.
 //
 
+import Foundation
 import Testing
 
 @testable import SyntaxKit
