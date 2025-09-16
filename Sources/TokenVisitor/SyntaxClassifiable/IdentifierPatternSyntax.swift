ac77aa12b210167225b3d78e54b2b4fc3d3c6c6e
Leo Dion <leogdion@brightdigit.com>
2025-09-16 13:30:03 -0400

    fix for windows cache

--- Sources/TokenVisitor/SyntaxClassifiable/IdentifierPatternSyntax.swift
+++ Sources/TokenVisitor/SyntaxClassifiable/IdentifierPatternSyntax.swift
@@ -30,7 +30,7 @@
 import SwiftSyntax
 
 extension IdentifierPatternSyntax: SyntaxClassifiable {
-  static var syntaxType: SyntaxType {
+  internal static var syntaxType: SyntaxType {
     .pattern
   }
 }
